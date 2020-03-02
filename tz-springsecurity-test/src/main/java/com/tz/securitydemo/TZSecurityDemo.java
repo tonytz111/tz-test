f11b848c3d0e11f5fe29365834723e9f886be542
tonytz111 <tonytz111@sina.com>
2020-03-02 14:39:02 +0800

    test

--- tz-springsecurity-test/src/main/java/com/tz/securitydemo/TZSecurityDemo.java
+++ tz-springsecurity-test/src/main/java/com/tz/securitydemo/TZSecurityDemo.java
@@ -11,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class TZSecurityDemo {
 
     public static void main(String[] args) {
+        //启动类
         SpringApplication.run(TZSecurityDemo.class, args);
     }
 
